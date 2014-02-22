c23bc2f1fd32736d0bbdb908df295f3a7bbd885e
Vlad Ureche <vlad.ureche@gmail.com>
2014-02-22 01:01:40 +0100

    Eugene's example
    you need Scala IDE for 2.11.0-M8 to load this project

--- components/macros/src/scaladyno/playground/macroz/package.scala
+++ components/macros/src/scaladyno/playground/macroz/package.scala
@@ -1,12 +1,31 @@
 package scaladyno.playground
 
-import scala.reflect.macros.Context
+import scala.reflect.macros.whitebox._
+import scala.language.implicitConversions
 import scala.language.experimental.macros
 
-package object macroz {
-  implicit def convert[T](a: Any): T = macro convertImpl[T]
-  def convertImpl[T: c.WeakTypeTag](c: Context)(a: c.Expr[Any]) : c.Expr[T] = {
-    println("type tag: "+ implicitly[c.WeakTypeTag[T]])
-    c.universe.reify[T](???.asInstanceOf[T])
+class SomethingWithBar { def bar: Int = 2 }
+
+class Foo[TFoo]
+object Foo {
+  implicit def materializeFoo[TMaterializeFoo]: Foo[TMaterializeFoo] = macro impl[TMaterializeFoo]
+
+  def impl[TFooImpl: c.WeakTypeTag](c: Context) = {
+    import c.universe._
+    println(showRaw(c.weakTypeOf[TFooImpl], printIds = true, printKinds = true))
+    println(c.weakTypeOf[TFooImpl].typeSymbol.getClass)
+    println(showRaw(c.weakTypeOf[TFooImpl].typeSymbol.typeSignature))
+    q"new scaladyno.playground.Foo[scaladyno.playground.SomethingWithBar]"
   }
 }
+
+package object macroz {
+  implicit def convert[TConvert: Foo](x: Any): TConvert = macro impl[TConvert]
+
+  def impl[TMacrosImpl: c.WeakTypeTag](c: Context)(x: c.Tree)(ev: c.Tree) = {
+    import c.universe._
+    println("xxxxxx: " + ev.tpe.asInstanceOf[TypeRef])
+    q"new ${ev.tpe.asInstanceOf[TypeRef].args.head}"
+  }
+}
+
--- project/Build.scala
+++ project/Build.scala
@@ -4,11 +4,11 @@ import Process._
 
 object ScaladynoBuild extends Build {
 
-  val scalaVer = "2.10.3"
+  val scalaVer = "2.11.0-M8"
 
   val defaults = Defaults.defaultSettings ++ Seq(
     scalaVersion := scalaVer,
-    scalaBinaryVersion := "2.10",
+    //scalaBinaryVersion := "2.11",
     scalaSource in Compile <<= baseDirectory(_ / "src"),
     javaSource in Compile <<= baseDirectory(_ / "src"),
     scalaSource in Test <<= baseDirectory(_ / "test"),
@@ -21,10 +21,10 @@ object ScaladynoBuild extends Build {
     //http://stackoverflow.com/questions/10472840/how-to-attach-sources-to-sbt-managed-dependencies-in-scala-ide#answer-11683728
     com.typesafe.sbteclipse.plugin.EclipsePlugin.EclipseKeys.withSource := true,
 
-    libraryDependencies ++= Seq(
-      "org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
-      "com.novocode" % "junit-interface" % "0.10-M2" % "test"
-    ),
+    //libraryDependencies ++= Seq(
+    //  "org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
+    //  "com.novocode" % "junit-interface" % "0.10-M2" % "test"
+    //),
 
     parallelExecution in Test := false,
     testOptions += Tests.Argument(TestFrameworks.JUnit, "-q", "-v"),
@@ -37,18 +37,17 @@ object ScaladynoBuild extends Build {
       "org.scala-lang" % "scala-library" % scalaVer,
       "org.scala-lang" % "scala-reflect" % scalaVer,
       "org.scala-lang" % "scala-compiler" % scalaVer,
-      "org.scala-lang" % "scala-partest" % scalaVer,
       "com.googlecode.java-diff-utils" % "diffutils" % "1.2.1"
     ),
 
     scalacOptions in Compile ++= Seq(
-      "-Ytyper-debug",
-      "-Ydebug",
-      "-Ymacro-debug-lite"
-    ),
+    //  "-Ytyper-debug",
+    //  "-Ydebug",
+    //  "-Ymacro-debug-lite"
+    )
 
-    addCompilerPlugin("org.scalamacros" %% "paradise" % "2.0.0-SNAPSHOT" cross CrossVersion.full),
-    libraryDependencies += "org.scalamacros" %% "quasiquotes" % "2.0.0-SNAPSHOT" cross CrossVersion.full
+    // addCompilerPlugin("org.scalamacros" %% "paradise" % "2.0.0-SNAPSHOT" cross CrossVersion.full)
+    // libraryDependencies += "org.scalamacros" %% "quasiquotes" % "2.0.0-SNAPSHOT" cross CrossVersion.full // qqs only needed for 2.10
   )
 
   lazy val _playground = Project(id = "playground",                base = file(".")) aggregate (macros, scratchpad)
